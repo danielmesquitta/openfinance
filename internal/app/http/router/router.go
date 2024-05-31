2ff802750f299f26fd6cd0feb258c8ba49850a8c
danielmesquitta <danielmesquitta123@gmail.com>
2024-05-30 21:14:04 -0300

    feat: add routes

--- internal/app/http/router/router.go
+++ internal/app/http/router/router.go
@@ -3,26 +3,31 @@ package router
 import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/swagger"
+	"github.com/shareed2k/goth_fiber"
 
 	"github.com/danielmesquitta/openfinance/internal/app/http/docs"
 	"github.com/danielmesquitta/openfinance/internal/app/http/handler"
+	"github.com/danielmesquitta/openfinance/internal/app/http/middleware"
 )
 
 type Router struct {
 	openFinanceToNotionHandler *handler.OpenFinanceToNotionHandler
+	authHandler                *handler.AuthHandler
+	settingHandler             *handler.SettingHandler
+	middleware                 *middleware.Middleware
 }
 
-// @title OpenFinance to Notion API
-// @version 1.0
-// @description This API is responsible for syncing OpenFinance data to Notion.
-// @contact.name Daniel Mesquita
-// @contact.email danielmesquitta123@gmail.com
-// @BasePath /
 func NewRouter(
 	openFinanceToNotionHandler *handler.OpenFinanceToNotionHandler,
+	authHandler *handler.AuthHandler,
+	settingHandler *handler.SettingHandler,
+	middleware *middleware.Middleware,
 ) *Router {
 	return &Router{
 		openFinanceToNotionHandler: openFinanceToNotionHandler,
+		authHandler:                authHandler,
+		settingHandler:             settingHandler,
+		middleware:                 middleware,
 	}
 }
 
@@ -32,6 +37,13 @@ func (r *Router) Register(
 	basePath := "/api/v1"
 
 	apiV1 := app.Group(basePath)
+
+	apiV1.Get("/auth/login/:provider", goth_fiber.BeginAuthHandler)
+	apiV1.Get("/auth/callback/:provider", r.authHandler.OAuthCallback)
+
+	apiV1.Use(r.middleware.EnsureAuthenticated)
+
+	apiV1.Post("/users/me/settings", r.settingHandler.Upsert)
 	apiV1.Post(
 		"/to-notion",
 		r.openFinanceToNotionHandler.Sync,
